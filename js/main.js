0c9d41cd3d21933b908308173d8895be2c0bcdc3
galitsky.e.o <galitsky.e.o@gmail.com>
2016-10-17 17:16:29 +0300

    Create resume block

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function() {
+	var pull 		= $('#pull');
+		menu 		= $('nav ul');
+		menuHeight	= menu.height();
+	$(pull).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function(){       //   RESIZE WINDOW FUNCTION
+		var w = $(window).width();
+		if(w > 320 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+});
+
+jQuery(document).ready(function($) {
+	// ANCHORS
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+	});
+
+	/*
+	var defaults = {
+			containerID: 'toTop', // fading element id
+		containerHoverID: 'toTopHover', // fading element hover id
+		scrollSpeed: 1200,
+		easingType: 'linear' 
+		};
+	*/
+	$().UItoTop({ easingType: 'easeOutQuart' });
+});
