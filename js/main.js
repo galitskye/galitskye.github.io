2525884acc6c3bdc202efe33dccb603db27ebb6d
galitsky.e.o <galitsky.e.o@gmail.com>
2017-03-07 21:21:08 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -31,16 +31,8 @@ jQuery(document).ready(function($) {
 	// ANCHORS
 	$(".scroll").click(function(event){		
 		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		$('html,body').animate({scrollTop:$(this.hash).offset().top - 80},1000);
 	});
-
-	/*
-	var defaults = {
-			containerID: 'toTop', // fading element id
-		containerHoverID: 'toTopHover', // fading element hover id
-		scrollSpeed: 1200,
-		easingType: 'linear' 
-		};
-	*/
+	
 	$().UItoTop({ easingType: 'easeOutQuart' });
 });
